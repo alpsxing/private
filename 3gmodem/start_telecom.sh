a61bd6e24274bb7c5b056614d508f4ad8e062aa0
Xing Qianqian <xingqq@xingqq-notepad.(none)>
2014-01-19 16:11:14 +0800

    Add more files.

--- 3gmodem/start_telecom.sh
+++ 3gmodem/start_telecom.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 while true; do
-    SERIAL_NUMBER=`cat /proc/tty/driver/usbserial | grep "vendor:12d1 product:1506" | sed -n 's/://'p | awk '{print $1}' | sed -n '1p'`
+    SERIAL_NUMBER=`cat /proc/tty/driver/usbserial | grep "vendor:12d1 product:140c" | sed -n 's/://'p | awk '{print $1}' | sed -n '1p'`
     if [ -z $SERIAL_NUMBER ]; then
-        echo "Unicom dongle not detected."
+        echo "Telecom dongle not detected."
         sleep 2
         continue
     fi
 
-     sed "s/\(^Modem\ *=\).*/\1\ \/dev\/ttyUSB${SERIAL_NUMBER}/" < /etc/loadshare/wvdial_unicom.conf > /var/wvdial_unicom.conf
-     wvdial -C /var/wvdial_unicom.conf
-     rm -f /var/wvdial_unicom.conf
+     sed "s/\(^Modem\ *=\).*/\1\ \/dev\/ttyUSB${SERIAL_NUMBER}/" < /etc//loadshare/wvdial_telecom.conf > /var/wvdial_telecom.conf
+     wvdial -C /var/wvdial_telecom.conf
+     rm -f /var/wvdial_telecom.conf
 #    LC_ALL=C  nmcli dev | grep "^ttyUSB${SERIAL_NUMBER}.*disconnected"
 #    if [ $? -eq 0 ]; then
 #        echo "Unicom dongle disconnected"
